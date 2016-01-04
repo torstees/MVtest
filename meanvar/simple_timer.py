c2ccf1739ef2b612f2c337944c1d63f8670099c8
Eric Torstenson <eric.torstenson@gmail.com>
2016-01-04 15:41:11 -0600

    Added copyright info

--- meanvar/simple_timer.py
+++ meanvar/simple_timer.py
@@ -1,6 +1,24 @@
 import time
 import sys
 
+__copyright__ = "Copyright (C) 2015 Todd Edwards, Chun Li and Eric Torstenson"
+__license__ = "GPL3.0"
+#     This file is part of MVtest.
+#
+#     MVtest is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     MVtest is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 class SimpleTimer:
     """Simple abstraction to allow for basic timing. """
 
