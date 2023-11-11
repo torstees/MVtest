a18b27685171e222276124a138426bfc3eb1caad
Eric Torstenson <eric.s.torstenson@vumc.org>
2023-11-11 15:36:58 -0600

    Removed import requirement for getting version info

--- setup.py
+++ setup.py
@@ -1,42 +1,39 @@
-import ez_setup
-ez_setup.use_setuptools()
+from setuptools import setup, find_packages, find_namespace_packages
 
-import setuptools
+version = {}
+with open("meanvar/version.py") as fp:
+    exec(fp.read(), version)
+
 import os
-import sys
 
-
-# Grab the version from the application itself for consistency
-import mvtest
-import meanvar
 
 # Use the README as the long description
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setuptools.setup(name="mvtest",
-    version=meanvar.__version__,
+setup(
+    name="mvtest",
+    version=version["__version__"],
     author="Eric Torstenson",
     author_email="eric.s.torstenson@vanderbilt.edu",
     url="https://github.com/edwards-lab/MVtest",
-    download_url="https://github.com/edwards-lab/MVtest/archive/v1.0.5.tar.gz",
-    packages=["meanvar","meanvar.tests"],
+    download_url="https://github.com/edwards-lab/MVtest/archive/v1.5.0.tar.gz",
+    packages=["meanvar", "meanvar.tests"],
     license="GPL",
     scripts=["mvtest.py", "scripts/mvmany.py"],
     description=["GWAS analysis"],
-    install_requires=["scipy","numpy","libgwas"],
-    long_description=read('README'),
-    keywords=["GWAS","genetic analysis"],
-    test_suite='meanvar.tests',
-    package_data={'meanvar/tests/bedfiles/':['*'],
-                  'doc':['*']},
+    install_requires=["scipy", "numpy", "libgwas"],
+    long_description=read("README"),
+    keywords=["GWAS", "genetic analysis"],
+    test_suite="meanvar.tests",
+    package_data={"meanvar/tests/bedfiles/": ["*"], "doc": ["*"]},
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Software Development :: Libraries",
-        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.7",
     ],
 )
